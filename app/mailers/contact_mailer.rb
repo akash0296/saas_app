5897ece5575ea0e0763d0ee078c68e0e9d728689
EC2 Default User <ec2-user@ip-172-31-12-245.us-east-2.compute.internal>
2018-02-06 14:44:41 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer::Base
     default to: 'akashmukherjee452@gmail.com'
     
-    def creat_email(name, email, body)
+    def contact_mailer(name, email, body)
        @name = name
        @email = email
        @body = body
